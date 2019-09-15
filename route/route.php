4f9d7ea39a184ee05801b3868f09db6379b4f0de
hf_yuan <hf_yuan@sina.com>
2019-09-15 16:48:36 +0800

    陈丁塬--更换仓库目录,切换环境到wamp

--- route/route.php
+++ route/route.php
@@ -1,4 +1,5 @@
 <?php
+
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
@@ -8,13 +9,23 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-
-Route::get('think', function () {
+//use think\Route;
+/*Route::get('think', function () {
     return 'hello,ThinkPHP5!';
-});
+});*/
 
-Route::get('hello/:name', 'index/hello');
+//Route::get('hello/:name', 'index/hello');
+Route::get('', 'index/index');
+Route::get('say', 'index/index/say');
+Route::rule("admin","admin/Admin/index");
+Route::get("main","sample/test/index");
+/*Route::group('admin', [
+    ':id'   => 'Admin/index',
+    ':name' => 'Blog/read',
+])->ext('html')->pattern(['id' => '\d+']);*/
+// 路由到默认或者绑定模块
 
+//Route::rule('admin/:id','admin/Admin/index');
 return [
 
 ];
